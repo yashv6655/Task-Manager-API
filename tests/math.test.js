da9af6b8fffb39152f0f9d67374e91834f90e5f1
Yash Velagapudi <yashv6655@gmail.com>
2021-05-04 16:08:14 -0700

    Used expect

--- tests/math.test.js
+++ tests/math.test.js
@@ -2,7 +2,5 @@ const { calculateTip } = require("../src/math");
 
 test("Total tip", () => {
   const total = calculateTip(10, 0.3);
-  if (total !== 13) {
-    throw new Error(`Total tip should be 13. Returned ${total}`);
-  }
+  expect(total).toBe(13);
 });
