24fc056159b6fd0aa7166db4c8ecf88e0ddc77b6
Yash Velagapudi <yashv6655@gmail.com>
2021-05-04 15:50:50 -0700

    Testing a simple math function

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,5 +1,8 @@
-test("hello", () => {});
+const { calculateTip } = require("../src/math");
 
-test("fail", () => {
-  throw new Error("fail");
+test("Total tip", () => {
+  const total = calculateTip(10, 0.3);
+  if (total !== 13) {
+    throw new Error(`Total tip should be 13. Returned ${total}`);
+  }
 });
