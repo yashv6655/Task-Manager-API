5e765d49da5059e7eeb9166580ec787b8ebbb6cc
Yash Velagapudi <yashv6655@gmail.com>
2021-05-05 21:00:11 -0700

    Tip function

--- src/math.js
+++ src/math.js
@@ -1,4 +1,4 @@
-const calculateTip = (total, tipPercent) => {
+const calculateTip = (total, tipPercent = 0.2) => {
   const tip = total * tipPercent;
   return total + tip;
 };
--- tests/math.test.js
+++ tests/math.test.js
@@ -4,3 +4,8 @@ test("Total tip", () => {
   const total = calculateTip(10, 0.3);
   expect(total).toBe(13);
 });
+
+test("Default total with tip", () => {
+  const total = calculateTip(10);
+  expect(total).toBe(12);
+});
