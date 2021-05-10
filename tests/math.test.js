d86d8696b1683b5bd70872084bd28fa7e527488d
Yash Velagapudi <yashv6655@gmail.com>
2021-05-10 15:29:27 -0700

    Temperature conversion tests

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,8 @@
-const { calculateTip } = require("../src/math");
+const {
+  calculateTip,
+  celsiusToFahrenheit,
+  fahrenheitToCelsius,
+} = require("../src/math");
 
 test("Total tip", () => {
   const total = calculateTip(10, 0.3);
@@ -8,4 +12,14 @@ test("Total tip", () => {
 test("Default total with tip", () => {
   const total = calculateTip(10);
   expect(total).toBe(12);
+});
+
+test("Convert 32 F to 0 C", () => {
+  const temp = fahrenheitToCelsius(32);
+  expect(temp).toBe(0);
+});
+
+test("Convert 0 C to 32 F", () => {
+  const temp = celsiusToFahrenheit(0);
+  expect(temp).toBe(32);
 });
